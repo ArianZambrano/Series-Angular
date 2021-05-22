a4fac872aee35f96e6acecc364593b2420cbf2c3
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2021-05-22 01:34:03 -0500

    ops: Setting firebaseConfig and budgets for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAN4Cnb050_iVA8LZkj3sAUQvwki-SehVI",
+    authDomain: "series-bc20d.firebaseapp.com",
+    projectId: "series-bc20d",
+    storageBucket: "series-bc20d.appspot.com",
+    messagingSenderId: "1004234224917",
+    appId: "1:1004234224917:web:68ed8539d0d995fe77a51c",
+    measurementId: "G-2566TVS2VT"
+  }
 };
